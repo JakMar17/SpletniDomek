c8c3fd2549fc4f73e87f94ceef18c06bfc0c40a9
jakmar17 <jakob.marusic17@gmail.com>
2021-08-04 20:10:00 +0200

    .env

--- api/posts.js
+++ api/posts.js
@@ -1,8 +1,8 @@
 import GhostContentAPI from '@tryghost/content-api'
 
 const api = new GhostContentAPI({
-  url: process.env.ghostUrl,
-  key: process.env.ghostKey,
+  url: process.env.GHOST_URL,
+  key: process.env.GHOST_KEY,
   version: 'v3',
 })
 
